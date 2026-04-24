7622f7fbe591b1157e1e41cf16e0d8f03a0aac6c
AshishManer <108808771+ash44444@users.noreply.github.com>
2026-04-24 19:08:54 +0530

    Ninth commit

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const ctrl = require("../controllers/product.controller");
 const auth = require("../middleware/auth.middleware");
-const validate = require("../middleware/validate.middleware");
 const { productSchema } = require("../validations/product.validation");
 
-router.post("/", auth, validate(productSchema), ctrl.add);
+router.post("/", auth, ctrl.add);
 router.get("/", auth, ctrl.get);
 router.get("/:id/pdf", auth, ctrl.getPDF);
 router.delete("/:id", auth, ctrl.delete);
