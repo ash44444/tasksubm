a168ff83d8932c03e74e644947dee217faf99efe
AshishManer <108808771+ash44444@users.noreply.github.com>
2026-04-24 19:05:50 +0530

    Seventh commit

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 exports.sendToken = (res, payload) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
   });
 
   return token;
