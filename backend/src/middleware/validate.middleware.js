f55149ff1677609b6652a31370e3d977da7a0bd6
AshishManer <108808771+ash44444@users.noreply.github.com>
2026-04-24 19:23:37 +0530

    commit

--- backend/src/middleware/validate.middleware.js
+++ backend/src/middleware/validate.middleware.js
@@ -1,11 +1,1 @@
-module.exports = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.body);
-    next();
-  } catch (err) {
-    res.status(400).json({
-      message: "Validation Error",
-      errors: err.errors,
-    });
-  }
-};
+
